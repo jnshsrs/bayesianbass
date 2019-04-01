9c67f0243c336c310cb05f33ae7f3c491e2af376
jnshsrs <jenshuesers@gmail.com>
2019-04-01 17:48:34 +0200

    added functions to predict and to plot the adoption and the diffusion

--- R/plot-bayesian-bass-model.R
+++ R/plot-bayesian-bass-model.R
@@ -21,7 +21,7 @@ plot.bayesian_bass <- function(x, se = F, ...) {
 
     pred <- predict(x)
 
-    adoption_rates <- x %>% pluck("adoption_rates")
+    adoption_rates <- x %>% pluck("diffusion_actual")
 
     p <- ggplot(pred, aes(x = t, y = predicted)) +
       geom_line() +
@@ -36,4 +36,65 @@ plot.bayesian_bass <- function(x, se = F, ...) {
 
     return(p)
 
+}
+
+#' Plot Diffusion Curve
+#'
+#' @param obj A Bayesian Bass model
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' data(adoption_data)
+#' model <- bayesian_bass_model()
+#' fit <- bayesian_bass(data = adoption_data, var = "adoption", model = model)
+#' fit_diffusion <- predict_diffusion(fit)
+#' plot(fit_diffusion)
+#'
+plot.bayesian_bass_diffusion <- function(obj, hdi = TRUE) {
+
+  x <- purrr::pluck(obj, "diffusion_data")
+
+  x <- x %>% mutate(time = as.integer(seq(nrow(.))))
+
+  adoption_rates <- purrr::pluck(obj, "diffusion_actual")
+
+  p <- x %>%
+    ggplot(aes(x = time, y = avg_adoption)) +
+    geom_line() +
+    geom_point(data = adoption_rates, aes(x = t, y = avg), inherit.aes = FALSE) +
+    scale_y_continuous("Adoption Rate", labels = scales::percent)
+
+  if (hdi) {
+    p +
+      geom_line(aes(x = time, y = hdi_lower), col = "grey") +
+      geom_line(aes(x = time, y = hdi_upper), col = "grey")
+  } else {
+    p
+  }
+
+}
+
+
+#' Plot distribution of adoption rate
+#'
+#' @param obj An object of class \code{bayesian_bass_adoption}
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' data(adoption_data)
+#' model <- bayesian_bass_model()
+#' fit <- bayesian_bass(data = adoption_data, var = "adoption", model = model)
+#' fit_adoption <- predict_adoption(fit, time = 1)
+#' plot(fit_adoption)
+#'
+plot.bayesian_bass_adoption <- function(obj) {
+  data <- tibble(adoption = unlist(obj))
+  data %>%
+    ggplot(aes(x = adoption)) +
+    geom_density() +
+    scale_x_continuous("Adoption Rate", label = scales::percent)
 }
--- R/predict-bass-model.R
+++ R/predict-bass-model.R
@@ -25,7 +25,7 @@ predict.bayesian_bass <- function(obj, intervals = NULL) {
   p <- coef(obj) %>% pluck("p")
   q <- coef(obj) %>% pluck("q")
 
-  predicted <- predict_bass(p, q, t = t)
+  predicted <- predict_bass(p, q, time = t)
 
   tibble::tibble(t = t, predicted = predicted)
 
@@ -60,9 +60,78 @@ posterior <- function(x, time_index = 1) {
 #' @return
 #'
 #' @examples
-predict_bass <- function(p, q, t) {
-  predicted <- (1 - exp(-(p + q) * t))/(1 + (q/p) * exp(-(p + q) * t))
+predict_bass <- function(p, q, time) {
+  predicted <- (1 - exp(-(p + q) * time))/(1 + (q/p) * exp(-(p + q) * time))
   return(predicted)
 }
+
+
+#' Predicts the adoption rate
+#'
+#' The adoption rate is defined as the proportion of adopters of a specific point of time
+#'
+#' @param obj An object of class \code{bayesian_bass}
+#' @param time An integer value specifing the point in time
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' data(adoption_data)
+#' model <- bayesian_bass_model()
+#' fit <- bayesian_bass(data = adoption_data, var = "adoption", model = model)
+#' predict_adoption(fit, time = 1)
+predict_adoption <- function(obj, time) {
+
+  coefs <- purrr::pluck(obj, "rjags_chains") %>% as.list()
+  x <- purrr::pmap_dbl(coefs, predict_bass, t = time)
+  class(x) <- "bayesian_bass_adoption"
+  return(x)
+}
+
+#' Predict the diffusion based on a bayesian bass model
+#'
+#' @param obj An object of class \code{bayesian_bass}
+#' @param time_index An integer vector conaining a sequence of time intervals
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' data(adoption_data)
+#' model <- bayesian_bass_model()
+#' fit <- bayesian_bass(data = adoption_data, var = "adoption", model = model)
+#' predict_diffusion(fit)
+predict_diffusion <- function(obj, time_index = NULL) {
+
+
+  actual_adoption_rates <- purrr::pluck(obj, "adoption_rates")
+  upper_limit <- max(purrr::pluck(obj, "data", "time"))
+  time_index <- seq(upper_limit)
+
+  diffusion <- purrr::map(.x = time_index, .f = predict_adoption, obj = obj)
+
+  data_diffusion <- tibble::tibble(avg_adoption = purrr::map_dbl(diffusion, mean),
+                                   sdev_adoption = purrr::map_dbl(diffusion, sd),
+                                   hdi = purrr::map(diffusion, function(x) (HDInterval::hdi(object = x)))) %>%
+    mutate(hdi = hdi %>% map(enframe)) %>%
+    unnest(hdi) %>%
+    spread(key = name, value = value) %>%
+    rename(hdi_lower = lower, hdi_upper = upper)
+
+  data <- list(
+    "diffusion_predicted" = diffusion,
+    "diffusion_data" = data_diffusion,
+    "diffusion_actual" = actual_adoption_rates
+  )
+
+  class(data) <- "bayesian_bass_diffusion"
+
+  return(data)
+
+}
+
+
+
 
 
